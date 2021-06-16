9eeb3bda9c7949114adc5ab54fd1d003eaae28ab
DarkRoop <67941985+DarkRoop@users.noreply.github.com>
2021-06-16 18:35:55 +0530

    Delete MyPopads.js

--- MyPopads.js
+++
@@ -1,8 +0,0 @@
-//<![CDATA[
-/* Night Mode/Dark Mode */ 
-
-$(document).ready(function(){$("body").toggleClass(localStorage.toggled),$("#modedark").on("click",function(){"nightmode"!=localStorage.toggled?($("body").toggleClass("nightmode",!0),localStorage.toggled="nightmode",$(".section-center").css("display","block")):($("body").toggleClass("nightmode",!1),localStorage.toggled="",$(".section-center").css("display",""))}),$("body").hasClass("nightmode")?$("#modedark").prop("checked",!0):$("#modedark").prop("checked",!1)});
-//]]>
-
-
-
